e3abd33d34e025867d3ca8025ceb445b1c462dce
Dogukan <dogukan.mertoglu01@gmail.com>
2024-12-02 17:46:38 +0100

    wip: trapez gust refactor and unit test

---
+++ tests/model_trapezoidal_gust_test.m
@@ -0,0 +1,17 @@
+time = linspace(0, 100); % s
+lat = zeros(length(time), 2);
+lat(:, 1) = time;
+lat(:, 2) = linspace(); % TODO
+lon = zeros(length(time), 2);
+lon(:, 1) = time;
+lon(:, 2) = linspace(); % TODO
+height = zeros(length(time), 2);
+height(:, 1) = time;
+height(:, 2) = linspace(); % TODO
+
+lat_0 = ;
+lon_0 = ;
+angle_entry = ;
+angle_exit = ;
+amplitude = ;
+width = ;
