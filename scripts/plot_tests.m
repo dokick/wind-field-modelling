9d0de750a4c836b99c6e59a27bd7e7fccb5d7cda
Dogukan Mertoglu <dogukan.mertoglu01@gmail.com>
2025-01-09 11:29:13 +0100

    test: added surf plot for trapez gust

--- scripts/plot_tests.m
+++ scripts/plot_tests.m
@@ -13,7 +13,7 @@ ylim([-0.5, amplitude(2)+1]);
 
 figure(2);
 plot(nearby.Values);
-xlim([100, 200]);
+xlim([130, 170]);
 ylim([-0.5, 1.1]);
 
 figure(3);
@@ -24,24 +24,22 @@ plot(w_lon.Values);
 
 figure(5);
 
-north_lla = ned2lla([gust_width(2)/2, 0, 0], [lat_0(2), lon_0(2), height_0(2)], "flat");
-south_lla = ned2lla([-gust_width(2)/2, 0, 0], [lat_0(2), lon_0(2), height_0(2)], "flat");
-east_lla = ned2lla([0, gust_width(2)/2, 0], [lat_0(2), lon_0(2), height_0(2)], "flat");
-west_lla = ned2lla([0, -gust_width(2)/2, 0], [lat_0(2), lon_0(2), height_0(2)], "flat");
-north = north_lla(1);
-south = south_lla(1);
-east = east_lla(2);
-west = west_lla(2);
+time_interval = [136, 164]; % s
+indexes = [17001, 20501];
+number_of_elements = indexes(2) - indexes(1) + 1;
 
-[lat_mesh, lon_mesh] = meshgrid(linspace(south, north), linspace(west, east));
+lat_relevant = lat(indexes(1):indexes(2), 2);
+lon_relevant = lon(indexes(1):indexes(2), 2);
+[lat_mesh, lon_mesh] = meshgrid(lat_relevant, lon_relevant);
 
-lat_vals = min([ ...
-    -gradient_north(2)*deg2rad(lat_mesh-north), ...
-    gradient_south(2)*deg2rad(lat_mesh-south), ...
-    amplitude]);
-lon_vals = min([ ...
-    -gradient_east(2)*deg2rad(lon_mesh-east), ...
-    gradient_west(2)*deg2rad(lon_mesh-west), ...
-    amplitude]);
+w_lat_relevant = w_lat.Values.Data(indexes(1):indexes(2));
+w_lon_relevant = w_lon.Values.Data(indexes(1):indexes(2));
 
-surf(lat_mesh, lon_mesh, min([lat_vals, lon_vals]));
+w_lat_relevant_mat = transpose(repmat(w_lat_relevant, 1, number_of_elements));
+w_lon_relevant_mat = repmat(w_lon_relevant, 1, number_of_elements);
+
+w_result = min(w_lat_relevant_mat, w_lon_relevant_mat);
+
+s = surf(rad2deg(lat_mesh), rad2deg(lon_mesh), w_result);
+shading interp
+colorbar
