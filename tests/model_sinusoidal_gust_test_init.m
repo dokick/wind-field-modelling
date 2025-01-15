80c5834e9b38ab35ef8b83826607ab8067f4be95
Dogukan Mertoglu <dogukan.mertoglu01@gmail.com>
2025-01-15 19:05:30 +0100

    wip: sinus gust impl; trapez gust optimization

--- tests/model_sinusoidal_gust_test_init.m
+++ tests/model_sinusoidal_gust_test_init.m
@@ -7,7 +7,7 @@ load("MyConfiguration.mat");
 FREQUENCY = 125; % Hz
 SIM_TIME = 300; % s
 
-time = linspace(0, SIM_TIME*FREQUENCY + 1); % s
+time = linspace(0, SIM_TIME, SIM_TIME*FREQUENCY + 1); % s
 
 lat = zeros(length(time), 2);
 lat(:, 1) = time;
@@ -27,12 +27,14 @@ altitude = zeros(length(time), 2);
 altitude(:, 1) = time;
 altitude(:, 2) = linspace(1000, 1000, length(time)); % m
 
+% The following parameters have to be 1x2, because they are scalar
+% and constant throughout the simulation
+
 lat_0 = [0, 52.5]; % deg
 lon_0 = [0, 13]; % deg
-height_0 = [0, 1000]; % rad
+height_0 = [0, 1000]; % m
 
-angle_entry = [0, deg2rad(45)]; % rad
-angle_exit = [0, deg2rad(40)];
-speed = [0, 5]; % m/s
+% The following paramters are specific to the phenomena
+
 amplitude = [0, 10]; % m/s
-width = [0, 1000]; % m
+gust_width = [0, 1000]; % m
