45248b93360001debc6ac2fb8427be4d3bd77988
Dogukan <dogukan.mertoglu01@gmail.com>
2025-08-28 15:52:46 +0200

    chore: deleted dead code

--- scripts/plot_path.m
+++ scripts/plot_path.m
@@ -42,8 +42,8 @@ sinusoidal_gust_params = transform_data(sinusoidal_gust_log_ids, sinusoidal_gust
 les_gust_params = transform_data(les_gust_log_ids, les_gust_capacity, out);
 cosine_gust_params = transform_data(cosine_gust_log_ids, cosine_gust_capacity, out);
 
-%% Run sim and plot
-% Running this section make sure the base models have restored their sample time otherwise this section will fail
+%% Run sim
+% Before running this section make sure the base models have restored their sample time otherwise this section will fail
 
 warning("off");
 
@@ -56,6 +56,7 @@ resolution = 100;
     length(trapezoidal_gust_log_ids), ...
     trapezoidal_gust_capacity, ...
     resolution);
+
 [total_sinus_instances, plot_values_sinus] = plot_instances_of_model( ...
     sinusoidal_gust_params, ...
     "model_sinusoidal_gust_w_g", ...
@@ -63,6 +64,7 @@ resolution = 100;
     length(sinusoidal_gust_log_ids), ...
     sinusoidal_gust_capacity, ...
     resolution);
+
 [total_les_instances, plot_values_les] = plot_instances_of_model( ...
     les_gust_params, ...
     "model_les_gust_w_g", ...
@@ -70,6 +72,7 @@ resolution = 100;
     length(les_gust_log_ids), ...
     les_gust_capacity, ...
     resolution);
+
 [total_cosine_instances, plot_values_cosine] = plot_instances_of_model( ...
     cosine_gust_params, ...
     "model_cosine_gust_w_g", ...
@@ -101,80 +104,6 @@ end
 for i = 1:total_cosine_instances
     s = surf(plot_values_cosine{i, 1}, plot_values_cosine{i, 2}, plot_values_cosine{i, 3});
     s.EdgeAlpha = 0;
-end
-
-hold off;
-set(h, "visible", "on");
-
-%% Run the simulations
-
-warning("off");
-
-model_type = "trapezoidal";
-resolution = 100;
-START_TIME = 0.0;
-SAMPLE_TIME = 0.008;
-
-h = figure("visible", "off");
-hold on;
-if length(lat) == resolution*resolution + 1
-    lat(end, :) = [];
-end
-if length(lon) == resolution*resolution + 1
-    lon(end, :) = [];
-end
-
-total_instances = 0;
-for i = 1:trapezoidal_gust_capacity
-    total_instances = total_instances + length(trapezoidal_gust_params{1, i});
-end
-
-current_instance = 0;
-for i = 1:trapezoidal_gust_capacity
-    current_params = trapezoidal_gust_params(:, i);
-%     current_lat_0 = trapezoidal_gust_params{1, i};
-%     current_lon_0 = trapezoidal_gust_params{2, i};
-%     current_alt_0 = trapezoidal_gust_params{3, i};
-%     current_gust_width = trapezoidal_gust_params{4, i};
-    num_instances = length(trapezoidal_gust_params{1, i});
-    % disp("Total instances in current slot: " + num_instances);
-    for j = 1:num_instances
-        % disp("Current instance: " + j);
-        current_values = zeros([1, num_params]);
-        for k = 1:num_params
-            current_values(k) = current_params{k, 1}(j);
-        end
-        lat_j = current_values(1);
-        lon_j = current_values(2);
-        alt_j = current_values(3);
-        gust_width_j = current_values(4)/2;
-        [north, south, east, west] = calculate_boundaries(gust_width_j, lat_j, lon_j, alt_j);
-        STOP_TIME = resolution*resolution*SAMPLE_TIME;
-        time = linspace(0, STOP_TIME-START_TIME, (STOP_TIME-START_TIME)*FREQUENCY+1);  % s
-        [lat_matrix, lon_matrix] = meshgrid(linspace(south, north, resolution), linspace(east, west, resolution));
-        lat(:, 1) = time(1:end-1);
-        lat(:, 2) = reshape(lat_matrix, [resolution*resolution, 1]);
-        lon(:, 1) = time(1:end-1);
-        lon(:, 2) = reshape(lon_matrix, [resolution*resolution, 1]);
-        alt = [0, alt_j];
-        lat_0 = [0, lat_j];
-        lon_0 = [0, lon_j];
-        alt_0 = [0, alt_j];
-        gust_width = [0, gust_width_j];
-        amplitude = [0, current_values(5)];
-        if model_type == "trapezoidal"
-            gradient_north = [0, current_values(6)];
-            gradient_south = [0, current_values(7)];
-            gradient_east = [0, current_values(8)];
-            gradient_west = [0, current_values(9)];
-        end
-        simOut = sim("model_trapezoidal_gust_test");
-        w_W_g = simOut.logsout.getElement("model_trapezoidal_gust_w").Values;
-        s = surf(lat_matrix, lon_matrix, reshape(w_W_g.Data(1:end-1), [resolution, resolution]));
-        s.EdgeAlpha = 0;
-        current_instance = current_instance + 1;
-        disp("Progress: " + current_instance/total_instances*100 + "%");
-    end
 end
 
 hold off;
