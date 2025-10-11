04a26933f975dcc06ceb597d26296a79cd66bc56
Dogukan <dogukan.mertoglu01@gmail.com>
2025-10-11 17:08:47 +0200

    fix: thermal gust def; test: added thermal gust test

---
+++ tests/model_thermal_gust_plot.m
@@ -0,0 +1,33 @@
+close all;
+
+w_g = out.logsout.getElement("model_thermal_gust_w_g").Values.Data;
+w_g = w_g(1:end-1);  % From 0s to 8000s leads to 10000001 elements, not compatible with reshape()
+w_g = reshape(w_g, size(lat_mesh));
+
+s = surf(lon_mesh, lat_mesh, w_g);
+s.EdgeAlpha = 0;
+cb = colorbar;
+cb.Label.String = "Windgeschwindigkeit [m/s]";
+cb.Label.FontSize = 15;
+xlabel("Längengrad (LON)", "FontSize", 15);
+ylabel("Breitengrad (LAT)", "FontSize", 15);
+effective_radius = gust_radius(2)*2.5;
+[no, so, ea, we] = calculate_boundaries(effective_radius, lat_0(2), lon_0(2), alt_0(2));
+limscale = 0.5;
+xlim([we - limscale*(ea-we), ea + limscale*(ea-we)]);
+ylim([so - limscale*(no-so), no + limscale*(no-so)]);
+% set(gca, 'xtick', []);
+% set(gca, 'ytick', []);
+
+figure;
+contourf(lon_mesh, lat_mesh, w_g);
+cb = colorbar;
+cb.Label.String = "Windgeschwindigkeit [m/s]";
+cb.Label.FontSize = 15;
+xlabel("Längengrad (LON)", "FontSize", 15);
+ylabel("Breitengrad (LAT)", "FontSize", 15);
+limscale = 0.3;
+xlim([we - limscale*(ea-we), ea + limscale*(ea-we)]);
+ylim([so - limscale*(no-so), no + limscale*(no-so)]);
+% set(gca, 'xtick', []);
+% set(gca, 'ytick', []);
---
+++ tests/model_thermal_gust_test_init.m
@@ -0,0 +1,64 @@
+% This is a unit test script and shouldn't fail, because of already defined variables in the workspace
+% That's why clear is called
+clear;
+
+load("MyConfigurationDyn.mat");
+
+FREQUENCY = 125;  % Hz
+START_TIME = 0;
+STOP_TIME = 8000;  % s
+SAMPLE_TIME = 1/FREQUENCY;  % s
+
+SIM_TIME = STOP_TIME - START_TIME;  % s
+
+time = linspace(0, SIM_TIME, SIM_TIME*FREQUENCY + 1);  % s
+
+lat_north = 52.5;
+lat_south = 52.45;
+lat = zeros(length(time), 2);
+lat(:, 1) = time;
+lat(:, 2) = linspace( ...
+    deg2rad(lat_north), ...
+    deg2rad(lat_south), ...
+    length(time));  % rad
+
+lon_east = 13.5;
+lon_west = 12;
+lon = zeros(length(time), 2);
+lon(:, 1) = time;
+lon(:, 2) = linspace( ...
+    deg2rad(lon_east), ...
+    deg2rad(lon_west), ...
+    length(time));  % rad
+
+alt = zeros(length(time), 2);
+alt(:, 1) = time;
+alt(:, 2) = linspace(1000, 1000, length(time));  % m
+
+EVENT_HORIZON = 1000;
+
+north_lla = ned2lla([EVENT_HORIZON, 0, 0], [lat_north, lon_east, 1000], "flat");
+north = north_lla(1);  % deg
+south_lla = ned2lla([-EVENT_HORIZON, 0, 0], [lat_north, lon_east, 1000], "flat");
+south = south_lla(1);  % deg
+east_lla = ned2lla([0, EVENT_HORIZON, 0], [lat_north, lon_east, 1000], "flat");
+east = east_lla(2);  % deg
+west_lla = ned2lla([0, -EVENT_HORIZON, 0], [lat_north, lon_east, 1000], "flat");
+west = west_lla(2);  % deg
+
+rng(0, "twister");
+
+% The following parameters have to be 1x2, because they are scalar
+% and constant throughout the simulation
+
+% lat_0 = [0, (north-south)*rand + south];  % deg
+lat_0 = [0, deg2rad(lat_north)];  % rad
+% lon_0 = [0, (east-west)*rand + west];  % deg
+lon_0 = [0, deg2rad(lon_east)];  % rad
+alt_0 = [0, 1000]; % m
+
+% The following paramters are specific to the phenomena
+
+gust_radius = [0, 200];  % m
+[~, ~, w_m] = calculate_amplitude(alt_0(2), gust_radius(2));
+amplitude = [0, w_m];  % m/s
